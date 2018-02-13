9f45205132d71e5fef14382f99546ff0c5244afa
Mattis <mattis_9509@hotmail.com>
2018-02-13 12:06:32 +0100

    Det här är allt jag gjort, spelet går att spela typ

---
+++ ADayAtStunlock/Assets/mattis prototyping/Scripts/dayTransition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayTransition : MonoBehaviour {
+
+    npcStats[] npcs;
+    float dayTimer;
+    public bool timePassing;
+
+	// Use this for initialization
+	void Start () {
+
+        timePassing = true;
+        dayTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        dayTimer += Time.deltaTime;
+
+
+        if(dayTimer > 30)
+        {
+
+            timePassing = false;
+            //bring up ui for end of day interaction
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            timePassing = true;
+            dayTimer = 0;
+        }
+
+	}
+}
---
+++ ADayAtStunlock/Assets/mattis prototyping/Scripts/playerStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStats : MonoBehaviour {
+
+    [HideInInspector]
+    public roomScript currentRoom;
+    dayTransition dayManager;
+
+
+
+    private void Start()
+    {
+        dayManager = FindObjectOfType<dayTransition>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+
+        if(dayManager.timePassing)
+        {
+            
+            if(Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                if(currentRoom != null)
+                {
+                    foreach(npcStats n in currentRoom.npcs)
+                    {
+                        print("Trying to boost mood of npcs in this room");
+                        n.happiness += 10;
+                        n.motivation += 10;
+                    }
+                }
+            }
+
+        }
+    }
+}
