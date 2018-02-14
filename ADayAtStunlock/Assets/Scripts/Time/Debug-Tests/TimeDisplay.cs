1b393a6027db59055a11863499143969374eba3a
Joel Åman <Schoelxz@gmail.com>
2018-02-14 15:11:51 +0100

    Implemented a namespace to easily find our created classes vs. default existing classes (from unity etc..).

--- ADayAtStunlock/Assets/Scripts/Time/Debug-Tests/TimeDisplay.cs
+++ ADayAtStunlock/Assets/Scripts/Time/Debug-Tests/TimeDisplay.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class TimeDisplay : MonoBehaviour {
-
-    private Text myText;
-    private int minutes;
-    private int hours;
-
-    public float timeSpeed = 1;
-    public int startMinute = 30;
-    public int startHour = 7;
-    
-    // Use this for initialization
-    void Start ()
+namespace DAS //  Namespace to sort out our own classes from other default classes.
+{
+    namespace DBUG
     {
-        myText = GetComponent<Text>();
-        TimeSystem.TimeMultiplier = timeSpeed;
-        TimeSystem.TimePassedSeconds = startMinute + (startHour*60);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        minutes = ((int)TimeSystem.TimePassedSeconds) % 60;
-        hours   = ((int)TimeSystem.TimePassedMinutes) % 24;
-        myText.text = hours.ToString("00") + ":" + minutes.ToString("00");
-    }
-}
+        
+        public class TimeDisplay : MonoBehaviour
+        {
+
+            private Text myText;
+            private int minutes;
+            private int hours;
+
+            public float timeSpeed = 1;
+            public int startMinute = 30;
+            public int startHour = 7;
+
+            // Use this for initialization
+            void Start()
+            {
+                myText = GetComponent<Text>();
+                TimeSystem.TimeMultiplier = timeSpeed;
+                TimeSystem.TimePassedSeconds = startMinute + (startHour * 60);
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                minutes = ((int)TimeSystem.TimePassedSeconds) % 60;
+                hours = ((int)TimeSystem.TimePassedMinutes) % 24;
+                myText.text = hours.ToString("00") + ":" + minutes.ToString("00");
+            }
+        }
+    } // Namespace DBUG
+} // Namespace DAS
