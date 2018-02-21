a8f8701709d4ac3b5a085042ca599b03159583c9
Joel Åman <Schoelxz@gmail.com>
2018-02-21 16:42:23 +0100

    Schedule begun.
    Test added scriptable objects for saving tasks to a schedule.

---
+++ ADayAtStunlock/Assets/Scripts/Schedule/Schedule.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Schedule", menuName = "Schedule/Task", order = 1)]
+public class TaskObject : ScriptableObject
+{
+    public float startTime, endTime;
+    public string taskName;
+}
+
+[CreateAssetMenu(fileName = "Schedule", menuName = "Schedule/Better Task", order = 1)]
+public class BetterTaskObject : ScriptableObject
+{
+    public Vector2[] TaskTime;
+    public string[] TaskName;
+}
+
+public class Schedule : MonoBehaviour
+{
+    public struct Task
+    {
+        private float startTime, endTime;
+        private string taskName;
+
+        public float StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        public float EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        public string TaskName
+        {
+            get { return taskName; }
+            set { taskName = value; }
+        }
+
+        public Task(float startTime, float endTime, string taskName)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.taskName = taskName;
+
+            if(this.startTime >= this.endTime)
+            {
+                // Not a valid event.
+            }
+        }
+
+    }
+
+    public List<Task> myTasks = new List<Task>();
+
+    public Task myCurrentTask;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < myTasks.Count; i++)
+        {
+            if(myTasks[i].StartTime >= DAS.TimeSystem.TimePassedSeconds && myTasks[i].EndTime < DAS.TimeSystem.TimePassedSeconds)
+            {
+                myCurrentTask = myTasks[i];
+            }
+        }
+	}
+
+}
---
+++ ADayAtStunlock/Assets/Scripts/Schedule/ScheduleManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScheduleManager : MonoBehaviour
+{
+
+    private List<Schedule> AllSchedules = new List<Schedule>();
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //DAS.TimeSystem.TimePassedSeconds % 1440;
+        //DAS.TimeSystem.TimePassedMinutes % 24;
+    }
+
+    public void AddScheduleTask(Schedule schedule, Schedule.Task task)
+    {
+        schedule.myTasks.Add(task);
+    }
+    public void AddScheduleTask(Schedule schedule, float startTime, float endTime, string taskName)
+    {
+        schedule.myTasks.Add(new Schedule.Task(startTime, endTime, taskName));
+    }
+
+    public void ModifyScheduleTask(Schedule.Task task)
+    {
+        
+    }
+
+    public void RemoveScheduleTask(Schedule schedule, Schedule.Task task)
+    {
+        schedule.myTasks.Remove(task);
+    }
+
+    public void GetSchedule()
+    {
+
+    }
+
+    private void CheckConflictingSchedule()
+    {
+        
+    }
+
+
+}
