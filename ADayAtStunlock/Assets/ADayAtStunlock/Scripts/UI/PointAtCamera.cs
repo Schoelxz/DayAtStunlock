5413ccb4599cfc428963950fe99d58e9798df01b
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-03-06 13:13:32 +0100

    renamed a method in the menuNavigatior. Added an NPC prefab with a panel that follows the camera. Added related pointatcamera script. Added a temporary sprite for the NPC and an icon.

---
+++ ADayAtStunlock/Assets/ADayAtStunlock/Scripts/UI/PointAtCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtCamera : MonoBehaviour {
+
+    public Camera camera;
+    // Use this for initialization
+    void Start () {
+        camera = FindObjectOfType<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        UpdateOrientationTowardsCamera();
+
+    }
+
+    void UpdateOrientationTowardsCamera()
+    {
+        gameObject.transform.LookAt(camera.transform);
+    }
+}
