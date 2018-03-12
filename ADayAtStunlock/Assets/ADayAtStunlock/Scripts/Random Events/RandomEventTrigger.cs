64032b0296e088e13ce57c56bcb141be5eb381e6
Mattis <mattis_9509@hotmail.com>
2018-03-12 16:03:41 +0100

    Created a thing that can trigger random events on a set interval, i also created a screen shake effect that can be used to simulate the train going by the office

---
+++ ADayAtStunlock/Assets/ADayAtStunlock/Scripts/Random Events/RandomEventTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEventTrigger : MonoBehaviour {
+
+    List<System.Action> randomEvents = new List<System.Action>(); //Add random event functions here
+    static public bool stopWork;
+    
+
+	// Use this for initialization
+	void Start () {
+        stopWork = false;
+        InvokeRepeating("TriggerRandomEvent", 5, 60);
+        randomEvents.Add(TrainEvent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+
+    void TriggerRandomEvent()
+    {
+        stopWork = true;
+
+        randomEvents[Random.Range(0, randomEvents.Count)]();
+    }
+
+
+    void TrainEvent()
+    {
+        ScreenShake.shakeDuration = 5;
+    }
+}
