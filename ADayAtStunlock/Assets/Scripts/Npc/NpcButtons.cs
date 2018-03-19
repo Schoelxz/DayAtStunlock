2996cfcdd3e987afe2fec1bf22a15eed39462b35
Mattis <mattis_9509@hotmail.com>
2018-03-19 16:03:29 +0100

    Created a script for disabling buttons when the player is far away, also added a script for buttons on the npc that can affect their motivation / happiness. (needs to be plugged in)

---
+++ ADayAtStunlock/Assets/Scripts/Npc/NpcButtons.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcButtons : MonoBehaviour {
+
+    Button[] buttons;
+
+    Button happiness;
+    Button motivation;
+    
+                
+	// Use this for initialization
+	void Start () {
+        buttons = GetComponentsInChildren<Button>(true);
+
+        foreach (var b in buttons)
+        {
+            if(b.name == "Happiness")
+            {
+                happiness = b;
+            }
+
+            if(b.name == "Motivation")
+            {
+                motivation = b;
+            }
+        }
+
+        if(motivation != null && happiness != null)
+        {
+            motivation.onClick.AddListener(AddMotivation);
+            happiness.onClick.AddListener(AddHappiness);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddMotivation()
+    {
+        print("Adds motivation to npc. (Actually doesnt, fix this)");
+    }
+
+    void AddHappiness()
+    {
+        print("Adds happiness to npc. (Actually doesnt, fix this)");
+    }
+}
