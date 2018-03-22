86b5dc23fe8df5b5ea6835a38f2b76c3546c9296
UTB\mattni <bo.niklas.mattiasson@gmail.com>
2018-03-22 11:54:16 +0100

    i made a scrikpt to be put on to a camra holder   that need to hold the camera (upenbart) with the camera pionting toward the camera the skript make the camra holder follow the player (if you remember to refernse the player) and turn left/right with the press of a button(Q/E)

--- ADayAtStunlock/Assets/Scripts/Camera/CameraMovement.cs
+++ ADayAtStunlock/Assets/Scripts/Camera/CameraMovement.cs
@@ -5,26 +5,73 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
     [SerializeField] private GameObject m_playerRef;
-    Vector3 cameraPostison;
+  /*  private Vector3 m_startRotasion;
+    private bool m_inRotasion;
+    private bool m_goingRight;
+    private bool m_goingLeft;
+    */
+    private Quaternion m_targetRotation;
+    [Header("this is ther camera rotasion speed")]
+    [Range(0.01f, 1f)]
+    [SerializeField]
+    private float speed = 0.1f;
+    private float m_rotationStep = 45f;
+
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+   
+    }
+
+    float dt;
+    // Update is called once per frame
+    void Update()
     {
-        this.transform.position = new Vector3 (m_playerRef.transform.position.x, m_playerRef.transform.position.y, m_playerRef.transform.position.z);
+        dt += Time.deltaTime;
+        this.transform.position = new Vector3(m_playerRef.transform.position.x, m_playerRef.transform.position.y, m_playerRef.transform.position.z);
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            this.transform.Rotate(this.transform.rotation.x, this.transform.rotation.y + 90, this.transform.rotation.z);
+            m_targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + m_rotationStep, transform.rotation.eulerAngles.z);
         }
-        else if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            this.transform.Rotate(Vector3.Lerp(new Vector3 (this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z), new Vector3(this.transform.rotation.x, this.transform.rotation.y - 90, this.transform.rotation.z), );
-            Vector3.Lerp();this.transform.rotation.x, this.transform.rotation.y - 90, this.transform.rotation.z);
+            m_targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - m_rotationStep, transform.rotation.eulerAngles.z);
         }
-        else { }
-	}
+        transform.rotation = Quaternion.Lerp(transform.rotation, m_targetRotation, speed);
+
+        /*if (Input.GetKeyDown(KeyCode.Q) && m_inRotasion==false)
+        {
+            m_goingLeft = true;
+            m_inRotasion = true;
+        }
+           
+
+        
+        else if (Input.GetKeyDown(KeyCode.E) && m_inRotasion == false)
+        {
+            m_goingRight = true;
+            m_inRotasion = true;
+        }
+        if (m_inRotasion == true && m_goingLeft == true)
+        {
+            this.transform.Rotate(Vector3.Lerp(new Vector3(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z),
+                                                new Vector3(this.transform.rotation.x, this.transform.rotation.y - 90, this.transform.rotation.z),
+                                                dt));
+        }
+
+
+        if (m_inRotasion == true && m_goingLeft == true)
+        {
+            this.transform.Rotate(Vector3.Lerp(new Vector3(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z),
+                                                          new Vector3(this.transform.rotation.x, this.transform.rotation.y + 90, this.transform.rotation.z),
+                                                          dt));
+        }
+        if (dt > 1) { 
+        dt = 0;
+            m_goingRight = false;
+            m_inRotasion = false;
+            m_goingLeft = false;
+        }*/
+    }
 }
