542793b63e35a192b439d3c921010fbeefa22ad9
Mattis <mattis_9509@hotmail.com>
2018-03-22 14:03:10 +0100

    did some small stuff

--- ADayAtStunlock/Assets/Scripts/Office/InteractableObject.cs
+++ ADayAtStunlock/Assets/Scripts/Office/InteractableObject.cs
@@ -7,15 +7,16 @@ public class InteractableObject : MonoBehaviour {
     float cooldown;
     float timer;
 
+    //Objects that are to be interacted with needs a collider, the "InteractableObject" tag, a proper name and a function for whatever purpose it should serve. See below.
+
 	// Use this for initialization
 	void Start () {
 
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-
+	void Update () {
+        
         if (PlayerRaycast.hit.transform != null && Input.GetMouseButtonDown(0))
         {
             if(PlayerRaycast.hit.transform.tag == "InteractableObject" )
