2c817a5a4251c22b3387f6a6e48c0605c5a02b26
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-03-22 13:46:12 +0100

    Updated player prefab with a model. Added unique materials to Player model. Updated scene with the new camera and player prefabs. Added player animator script to animate the player's movement.

---
+++ ADayAtStunlock/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimator : MonoBehaviour {
+    private Animator m_animator;
+    private NavMeshAgent agentRef;
+    // Use this for initialization
+    void Start ()
+    {
+        //Find Animator
+        m_animator = GetComponentInChildren<Animator>();
+        agentRef= GetComponent<NavMeshAgent>();
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movement Animation
+        m_animator.SetFloat("MoveSpeed", agentRef.velocity.magnitude);
+    }
+    
+}
