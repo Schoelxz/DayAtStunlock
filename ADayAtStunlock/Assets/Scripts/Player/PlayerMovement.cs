b04fced99c1a854311ca84f82686ec3d2d801d31
Joel Åman <Schoelxz@gmail.com>
2018-03-22 14:32:54 +0100

    The debug log of hit point was annoying so i removed it.

--- ADayAtStunlock/Assets/Scripts/Player/PlayerMovement.cs
+++ ADayAtStunlock/Assets/Scripts/Player/PlayerMovement.cs
@@ -21,7 +21,6 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        Debug.Log(PlayerRaycast.hit.point);
         if ( Input.GetMouseButton(0))
         {
             m_moveHere = PlayerRaycast.hit.point;
