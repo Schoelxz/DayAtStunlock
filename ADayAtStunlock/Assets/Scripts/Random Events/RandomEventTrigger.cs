6cb7356d54785fd68a5cfc222003c6ac36d597c7
Mattis <mattis_9509@hotmail.com>
2018-03-26 14:29:06 +0200

    Screen shake / train random event now sets all motivation to 0 and after the train has passed the motivation goes back up to its old value

--- ADayAtStunlock/Assets/Scripts/Random Events/RandomEventTrigger.cs
+++ ADayAtStunlock/Assets/Scripts/Random Events/RandomEventTrigger.cs
@@ -6,12 +6,12 @@ public class RandomEventTrigger : MonoBehaviour {
 
     List<System.Action> randomEvents = new List<System.Action>(); //Add random event functions here
     static public bool stopWork;
-    
+    List<float> motivationList = new List<float>();
 
 	// Use this for initialization
 	void Start () {
         stopWork = false;
-        InvokeRepeating("TriggerRandomEvent", 5, 60);
+        InvokeRepeating("TriggerRandomEvent", 60, 60);
         randomEvents.Add(TrainEvent);
 	}
 	
@@ -23,7 +23,6 @@ public class RandomEventTrigger : MonoBehaviour {
 
     void TriggerRandomEvent()
     {
-        stopWork = true;
 
         randomEvents[Random.Range(0, randomEvents.Count)]();
     }
@@ -32,5 +31,21 @@ public class RandomEventTrigger : MonoBehaviour {
     void TrainEvent()
     {
         ScreenShake.shakeDuration = 5;
+
+        foreach (var npc in DAS.NPC.s_npcList)
+        {
+            motivationList.Add(npc.myFeelings.Motivation);
+            npc.myFeelings.Motivation = 0;
+        }
+
+        Invoke("ResetMotivation", 5);
+    }
+
+    void ResetMotivation()
+    {
+        for(int i = 0; i < DAS.NPC.s_npcList.Count; i++)
+        {
+            DAS.NPC.s_npcList[i].myFeelings.Motivation = motivationList[i];
+        }
     }
 }
