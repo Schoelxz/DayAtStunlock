4a05b74bcbbc5fdb185131c6d9aa3af07f2442e4
Joel Åman <Schoelxz@gmail.com>
2018-03-27 12:42:25 +0200

    Made NPC buttons easier to press. Changed NPC prebab Changed NpcButtons.cs
    BUG: npc buttons can be seen in a frame when NPC spawns.

--- ADayAtStunlock/Assets/Scripts/Npc/UI/NpcButtons.cs
+++ ADayAtStunlock/Assets/Scripts/Npc/UI/NpcButtons.cs
@@ -5,49 +5,85 @@ using UnityEngine.UI;
 
 public class NpcButtons : MonoBehaviour {
 
-    Button[] buttons;
-    
-    Button happiness;
-    Button motivation;
-    
-                
-	// Use this for initialization
-	void Start () {
-        buttons = GetComponentsInChildren<Button>(true);
+    private Button[] m_buttons;
 
-        foreach (var b in buttons)
+    private Canvas m_buttonCanvas;
+
+    private RectTransform m_buttonHolder;
+    private Vector2 m_holderPos;
+
+    private Button m_happinessButton;
+    private Button m_motivationButton;
+
+    private DAS.NPC m_npcRef;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_buttons = GetComponentsInChildren<Button>(true);
+        // get npc reference
+        m_npcRef = GetComponent<DAS.NPC>();
+        // get canvas holding buttons
+        m_buttonCanvas = transform.GetChild(2).GetComponent<Canvas>();
+        // get button holder UI object.
+        m_buttonHolder = m_buttonCanvas.transform.GetChild(0).GetComponent<RectTransform>();
+
+        foreach (var b in m_buttons)
         {
             if(b.name == "Happiness")
             {
-                happiness = b;
+                m_happinessButton = b;
             }
 
             if(b.name == "Motivation")
             {
-                motivation = b;
+                m_motivationButton = b;
             }
         }
 
-        if(motivation != null && happiness != null)
+        if(m_motivationButton != null && m_happinessButton != null)
         {
-            motivation.onClick.AddListener(AddMotivation);
-            happiness.onClick.AddListener(AddHappiness);
+            m_motivationButton.onClick.AddListener(AddMotivation);
+            m_happinessButton.onClick.AddListener(AddHappiness);
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    private void Update()
+    {
+        if (Time.timeScale == 0)
+            m_buttonCanvas.enabled = false;
+        else
+            m_buttonCanvas.enabled = true;
+
+        // Make buttons follow NPCs in the screen overlay canvas.
+        if (m_npcRef != null && m_happinessButton.enabled == true)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(m_buttonCanvas.transform as RectTransform,
+                Camera.main.WorldToScreenPoint(m_npcRef.transform.position),
+                m_buttonCanvas.worldCamera,
+                out m_holderPos);
+
+            m_holderPos += new Vector2(Screen.width/2f, Screen.height/2f); // add some fixing offset to the buttons position.
+        }
+    }
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        // Make buttons follow NPCs in the screen overlay canvas.
+        if (m_npcRef != null && m_happinessButton.enabled == true)
+        {
+            m_buttonHolder.position = m_holderPos;
+        }
 	}
 
     void AddMotivation()
     {
-        GetComponent<DAS.NPC>().myFeelings.Motivation++;
+        m_npcRef.myFeelings.Motivation++;
     }
 
     void AddHappiness()
     {
-        GetComponent<DAS.NPC>().myFeelings.Happiness++;
+        m_npcRef.myFeelings.Happiness++;
     }
 }
 
