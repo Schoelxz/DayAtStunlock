3821401c3c4e11affccb581d123fc3dea3104717
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-04-03 17:09:32 +0200

    Added sound class and audioManager. Can be used in its current state however I am connecting it to the sliders in the main menu.

---
+++ ADayAtStunlock/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    [Range(0f, 1f)]
+    static public float masterVolume = 1f;
+    [Range(0f, 1f)]
+    static public float volumeSound = 1f;
+    [Range(0f, 1f)]
+    static  public float volumeMusic = 1f;
+
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+	void Awake () {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+
+		foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            switch(s.audioType)
+            {
+                case Sound.AudioType.music:
+                    s.source.volume = s.volume * volumeMusic * masterVolume;
+                break;
+
+                case Sound.AudioType.soundEffect:
+                    s.source.volume = s.volume * volumeSound * masterVolume;
+                break;
+                default:
+                    s.source.volume = s.volume * masterVolume;
+                    Debug.LogWarning("Unrecognized audioType" + s.audioType);
+                    break;
+            }
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+    void Start()
+    {
+        Play("Theme");
+    }
+    public void Play(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sounds: " + name + "not found!");
+            return;
+        }
+            
+        s.source.Play();
+	}
+}
---
+++ ADayAtStunlock/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+
+    [Range(0.1f, 3f)]
+    public float pitch = 1f;
+
+    public bool loop;
+    [HideInInspector]
+    public enum AudioType { music, soundEffect, test};
+    public AudioType audioType;
+    [HideInInspector]
+    public AudioSource source;
+
+
+}
