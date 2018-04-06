b10874f80aa2d91ddfe6fdd5cbe0d573e2babc28
Joel Åman <Schoelxz@gmail.com>
2018-04-06 14:38:57 +0200

    Made Arrows even better.
    Set raycast target on image to false (to avoid conflict with movement).
    Added some gizmo helper (editor play only).
    Some more stuff generally better and fine tuned.
    
    Removed old Arrow.png
    
    Added static player gameobject ref inside PlayerMovement.

--- ADayAtStunlock/Assets/Scripts/Npc/UI/FindNpcInUI.cs
+++ ADayAtStunlock/Assets/Scripts/Npc/UI/FindNpcInUI.cs
@@ -55,7 +55,7 @@ namespace DAS
                 get
                 {
                     if (
-                            myVector2.x > Screen.width / (FindNpcInUI.myInstance.screenClampMultiplier + myInstance.screenInsideBoundaryAllowance)
+                           myVector2.x > Screen.width / (FindNpcInUI.myInstance.screenClampMultiplier + myInstance.screenInsideBoundaryAllowance)
                         && myVector2.x < Screen.width - (Screen.width / (FindNpcInUI.myInstance.screenClampMultiplier + myInstance.screenInsideBoundaryAllowance))
                         && myVector2.y > Screen.height / (FindNpcInUI.myInstance.screenClampMultiplier + myInstance.screenInsideBoundaryAllowance)
                         && myVector2.y < Screen.height - (Screen.height / (FindNpcInUI.myInstance.screenClampMultiplier + myInstance.screenInsideBoundaryAllowance))
@@ -127,6 +127,7 @@ namespace DAS
                 m_icons.Add(new Icon(new GameObject("NPC Icon " + i)));
                 m_icons[i].MyGameObject.transform.parent = transform;
                 m_icons[i].MyGameObject.AddComponent<Image>().sprite = imageToDisplay;
+                m_icons[i].MyGameObject.GetComponent<Image>().raycastTarget = false;
                 m_icons[i].MyGameObject.GetComponent<Image>().rectTransform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 m_icons[i].MyGameObject.GetComponent<Image>().color = new Color(0.5f, 0, 0.5f, 1);
             }
@@ -170,25 +171,83 @@ namespace DAS
 
         private void LateUpdate()
         {
+            Vector2 screenVector2 = new Vector2(Screen.width, Screen.height);
             // Update all icon arrows; their position and if visible(and active)
             foreach (var icon in m_icons)
             {
+                icon.MyGameObject.SetActive(false);
+                if (icon.myNpcRef == null)
+                    continue;
+
                 //Set allowed position to keep inside
-                Vector3 allowedPos = new Vector3(icon.MyVector2.x, icon.MyVector2.y) - middlePoint;
+                Vector3 allowedPos = (Vector3)icon.MyVector2 - middlePoint;
                 allowedPos = Vector3.ClampMagnitude(allowedPos, Screen.height / (2f * screenClampMultiplier));
 
                 //Set gameobjects position to the Vector2 Position it should have.
                 icon.MyGameObject.transform.position = middlePoint + allowedPos;
-                icon.MyGameObject.SetActive(false);
-
-                if (icon.myNpcRef == null)
-                    continue;
 
                 if (icon.myNpcRef != null && icon.myNpcRef.myFeelings.TotalFeelings <= 0)
                     icon.MyGameObject.SetActive(true);
                 else
                     continue;
 
+                Vector2 npcPosInUIPos; //Npc position in UI Position (camera decided)
+                //Get position from the 3D world and convert it into a 2D position on the screen
+                RectTransformUtility.ScreenPointToLocalPointInRectangle
+                (
+                    m_myCanvas.transform as RectTransform,
+                    Camera.main.WorldToScreenPoint(icon.myNpcRef.transform.position),
+                    m_myCanvas.worldCamera,
+                    out npcPosInUIPos
+                );
+                Vector2 screenPos;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle
+                (
+                    m_myCanvas.transform as RectTransform,
+                    Camera.main.WorldToScreenPoint(new Vector2(Screen.width/2, Screen.height/2)),
+                    m_myCanvas.worldCamera,
+                    out screenPos
+                );
+
+                //Positioning of the Icons
+                npcPosInUIPos += new Vector2(Screen.width / 2f, Screen.height / 2f); // add some fixing offset.;
+                //npcPosInUIPos = new Vector2(npcPosInUIPos.x, Mathf.Clamp(npcPosInUIPos.y, -500, 500));
+
+                //Lerp a color by checking deistance between middlepoint of screen and npc position in 2D space
+                float distColLerp = Mathf.Clamp(1 - Vector3.Distance(PlayerMovement.s_playerGoRef.transform.position, icon.myNpcRef.transform.position) / (new Vector2(Screen.width, Screen.height).magnitude/70), 0.2f, 0.8f);
+                
+                //if the distance between the npc 2D-Pos and that npcs' icon position is less than "distanceTilArrowPlaced"
+                //if (Vector3.Distance(npcPosInUIPos, icon.MyGameObject.transform.position) < distanceTilArrowPlaced)
+                if(Screen.safeArea.Contains(npcPosInUIPos))
+                {
+                    if (!showArrowWhenClose)
+                    {
+                        icon.MyGameObject.SetActive(false);
+                        continue;
+                    }
+                    //Set icon position roughly above the head of the NPC
+                    icon.MyGameObject.transform.position = (Vector3)icon.MyVector2 + new Vector3(0, 80);
+                    icon.MyGameObject.GetComponent<Image>().color = new Color(0.9f, 0.15f, 0.15f, 1);//Arrow color change
+
+                    //if the new position of the Icon goes outside of the screen; set the position to its "original" position.
+                    if (!Screen.safeArea.Contains((icon.MyVector2 + new Vector2(0, 100))))
+                    {
+                        icon.MyGameObject.transform.position = (middlePoint + allowedPos) - new Vector3(0, 20);
+                        icon.MyGameObject.GetComponent<Image>().color = new Color(distColLerp, 0, 1 - distColLerp, 1); //Arrow color change
+                    }
+                }
+                else
+                    icon.MyGameObject.GetComponent<Image>().color = new Color(distColLerp, 0, 1 - distColLerp, 1); //Arrow color change
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            foreach (var icon in m_icons)
+            {
+                if (icon.myNpcRef == null)
+                    continue;
                 Vector2 npcPosInUIPos; //Npc position in UI Position
 
                 //Get position from the 3D world and convert it into a 2D position on the screen
@@ -200,41 +259,16 @@ namespace DAS
                     out npcPosInUIPos
                 );
 
-                //Positioning of the Icons
-                npcPosInUIPos += new Vector2(Screen.width / 2f, Screen.height / 2f); // add some fixing offset.;
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(icon.MyVector2, 100F);
 
-                //Lerp a color by checking deistance between middlepoint of screen and npc position in 2D space
-                float distColLerp = Mathf.Clamp(1 - Vector3.Distance(middlePoint, icon.MyVector2) / 1500, 0.2f, 0.8f);
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(npcPosInUIPos, 100F);
 
-                //if the distance between the npc 2D-Pos and that npcs' icon position is less than "distanceTilArrowPlaced"
-                if (Vector3.Distance(npcPosInUIPos, icon.MyGameObject.transform.position) < distanceTilArrowPlaced)
-                {
-                    if (!showArrowWhenClose)
-                    {
-                        icon.MyGameObject.SetActive(false);
-                        continue;
-                    }
-                    //Set icon position roughly above the head of the NPC
-                    icon.MyGameObject.transform.position = (Vector3)icon.MyVector2 + new Vector3(0, 110);
-                    icon.MyGameObject.GetComponent<Image>().color = new Color(0.9f, 0.15f, 0.15f, 1);//Arrow color change
-                }
-                else
-                    icon.MyGameObject.GetComponent<Image>().color = new Color(distColLerp, 0, 1 - distColLerp, 1); //Arrow color change
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(icon.MyGameObject.transform.position, 20F);
             }
         }
-
-
-        private void OnDrawGizmos()
-        {
-            Camera camera = Camera.main;
-            Vector3 p = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(p, 0.1F);
-
-            UnityEditor.Handles.color = Color.red;
-
-
-
-        }
+#endif
     }
 }
