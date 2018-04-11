e9febc60eb8f9dcc0fcd164548a0929ba2612096
Joel Åman <Schoelxz@gmail.com>
2018-04-11 16:07:40 +0200

    Ljus.
    Lade till flera ljuskällor och ändrade ljus options.
    Ändrade på FloorMat materialet så det inte visar spekulärt ljus samt är lite mörkare.
    Lade till ett skript som ökar dir ljus intensiteten tills den når 1, från mörkt till ljust då, i starten av spelet.

---
+++ ADayAtStunlock/Assets/Scripts/Light/DirLightIntro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirLightIntro : MonoBehaviour
+{
+
+    Light dirLight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dirLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (dirLight.intensity < 1)
+            dirLight.intensity += DAS.TimeSystem.DeltaTime / 10;
+        else
+            Destroy(this);
+		
+	}
+}
