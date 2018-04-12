a6874575321ee6087b1fc075f19785ef932c541d
Joel Åman <Schoelxz@gmail.com>
2018-04-12 17:45:14 +0200

    Updated script. updated scene with the camera rot.

--- ADayAtStunlock/Assets/Scripts/UI-Related/ArrowPointer.cs
+++ ADayAtStunlock/Assets/Scripts/UI-Related/ArrowPointer.cs
@@ -10,6 +10,7 @@ public class ArrowPointer : MonoBehaviour
     public Sprite typeOfSprite;
     private List<RectTransform> m_listOfArrows = new List<RectTransform>();
     private Canvas m_myCanvas;
+    private Vector3 middlePoint;
 
 
     #endregion
@@ -34,22 +35,48 @@ public class ArrowPointer : MonoBehaviour
 
     private void Update()
     {
+        // Point in the middle of the screen
+        middlePoint = new Vector3(Screen.width / 2, Screen.height / 2);
+
         for (int i = 0; i < listOfStuffToPointAt.Count; i++)
         {
             //2D canvas-positioning of a 3D object-world
-            Vector2 pos;
+            Vector2 stuffPos;
 
+            //Transform 3D-position to a 2D-canvas postiopn
             RectTransformUtility.ScreenPointToLocalPointInRectangle(m_myCanvas.transform as RectTransform,
                 Camera.main.WorldToScreenPoint(listOfStuffToPointAt[i].transform.position),
                 m_myCanvas.worldCamera,
-                out pos);
+                out stuffPos);
 
             //Positioning of the Icons
-            pos += new Vector2(Screen.width / 2f, Screen.height / 2f); // add some fixing offset.
+            stuffPos += new Vector2(Screen.width / 2f, Screen.height / 2f); // add some fixing offset.
 
-            m_listOfArrows[i].position = pos;
+            //Set allowed position to keep inside
+            Vector3 allowedPos = (Vector3)stuffPos - middlePoint;
+            allowedPos = Vector3.ClampMagnitude(allowedPos, Screen.height / (2f * 3f));
+
+            //Set arrow position to the Vector2 Position it should have but clamping it within a .
+            m_listOfArrows[i].position = middlePoint + allowedPos;
+
+            //Rotate the arrow towards the object it follows
+            RotateArrowTowardsTarget(stuffPos, m_listOfArrows[i]);
+
+            //Hide arrows when destination is reached.
+            if ((Vector2)m_listOfArrows[i].position == stuffPos)
+                m_listOfArrows[i].gameObject.SetActive(false);
+            else
+                m_listOfArrows[i].gameObject.SetActive(true);
         }
+    }
 
+    private void RotateArrowTowardsTarget(Vector2 targetPos, RectTransform arrow)
+    {
+        // Rotation of the Icons
+        Vector3 diff = (Vector3)targetPos - arrow.transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 
     private void LateUpdate()
