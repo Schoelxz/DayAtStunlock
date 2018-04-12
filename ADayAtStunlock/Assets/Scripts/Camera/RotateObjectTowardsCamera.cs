6dc2a9aaef047c114324bde2b2c4d5492c92add5
Joel Åman <Schoelxz@gmail.com>
2018-04-12 14:58:19 +0200

    Updated radiator prefab. Added script.
    Radiator prefabs now has a new script on them (on the canvas gameobject), this will rotate the button towards the camera at all times.
    
    Script added "RotateObjectTowardsCamera" is a script which rotates the objects forward towards the cameras forward transform.
    The script has a few inspector variables if needed to tweak the rotation and reversity of the object.

---
+++ ADayAtStunlock/Assets/Scripts/Camera/RotateObjectTowardsCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectTowardsCamera : MonoBehaviour
+{
+    public bool reverse, spinZ180, spinY180, spinX180;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+    void Update ()
+    {
+        if(reverse)
+            transform.forward = -Camera.main.transform.forward;
+        else
+            transform.forward = Camera.main.transform.forward;
+
+        if(spinZ180)
+            transform.Rotate(0, 0, 180);
+        if (spinY180)
+            transform.Rotate(0, 180, 0);
+        if (spinX180)
+            transform.Rotate(180, 0, 0);
+    }
+}
