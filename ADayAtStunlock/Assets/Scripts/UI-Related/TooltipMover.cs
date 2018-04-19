645ba54a94ae16cb524f454fba89d5a5b69008b1
DESKTOP-4362A26\nikla <bo.niklas.mattiasson@gmail.com>
2018-04-19 17:14:51 +0200

    semi-working but not

--- ADayAtStunlock/Assets/Scripts/UI-Related/TooltipMover.cs
+++ ADayAtStunlock/Assets/Scripts/UI-Related/TooltipMover.cs
@@ -14,7 +14,31 @@ public class TooltipMover : MonoBehaviour {
 
     // Update is called once per frame
     void Update ()
-    {
-        transform.position = Input.mousePosition + offset;
-	}
+    {   
+        //top right
+        if (Input.mousePosition.y <= Screen.height/2 && Input.mousePosition.x >= Screen.width /2)
+        {
+            
+        } 
+        // lower right
+        else if (Input.mousePosition.y >= Screen.height / 2 && Input.mousePosition.x >= Screen.width / 2)
+        {
+            offset.y = offset.y * -1;
+        }
+        //top left
+        else if (Input.mousePosition.y <= Screen.height / 2 && Input.mousePosition.x <= Screen.width / 2)
+        {
+            offset.x = offset.x * -1;
+            
+        }
+        //lower left
+        else if (Input.mousePosition.y >= Screen.height / 2 && Input.mousePosition.x <= Screen.width / 2)
+        {
+            offset.x = offset.x * -1;
+            offset.y = offset.y * -1;
+        }
+       
+
+     transform.position = Input.mousePosition + offset;
+    }
 }
