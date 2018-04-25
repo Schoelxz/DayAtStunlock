49e34fc6ea83ee2f84d7f3a3468b3af9e23eac22
Mattis <mattis_9509@hotmail.com>
2018-04-25 11:14:30 +0200

    Created a current score UI and a script that controls it, added it to the main menu scene aswell.

---
+++ ADayAtStunlock/Assets/Scripts/Score/UIScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour {
+
+    Text score;
+
+	// Use this for initialization
+	void Start () {
+        score = gameObject.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score.text = MoneyManager.moneyEarned.ToString("n0");
+	}
+}
