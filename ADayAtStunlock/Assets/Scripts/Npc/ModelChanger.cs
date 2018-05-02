75533842b2a69ce3537e921a562d5700fe5c1165
Joel Åman <Schoelxz@gmail.com>
2018-05-02 12:49:31 +0200

    Changed alien duration time.
    ModelChanger script has a public artisté variable to change the duration on how long a person is an alien.
    
    Fixed bug where randomevents are never cleared from the list, which adds to an infinite amount of delegates to the list.

--- ADayAtStunlock/Assets/Scripts/Npc/ModelChanger.cs
+++ ADayAtStunlock/Assets/Scripts/Npc/ModelChanger.cs
@@ -2,33 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ModelChanger : MonoBehaviour {
-
+public class ModelChanger : MonoBehaviour
+{
     private GameObject alienModel;
     private GameObject personModel;
     private DAS.NPCMovement npcMovement;
 
+    [Range(1, 120)]
+    public int stayAsAlienDuration = 45;
+
     public bool isAlien;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         isAlien = false;
         alienModel = gameObject.transform.GetChild(1).gameObject;
         personModel = gameObject.transform.GetChild(0).gameObject;
     }
-	
 
     public void ToggleModel()
     {
         npcMovement = gameObject.GetComponent<DAS.NPCMovement>();
         
-            isAlien = true;
-            personModel.SetActive(false);
-            alienModel.SetActive(true);
-            npcMovement.ToggleAnimator();
+        isAlien = true;
+        personModel.SetActive(false);
+        alienModel.SetActive(true);
+        npcMovement.ToggleAnimator();
 
-            Invoke("AlienOff", 90);
-        
+        Invoke("AlienOff", stayAsAlienDuration);
     }
 
     private void AlienOff()
