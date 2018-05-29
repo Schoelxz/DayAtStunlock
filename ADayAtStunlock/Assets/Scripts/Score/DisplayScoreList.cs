3a520d2447172540a9647a87f7ea6829846624d5
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-05-29 14:34:09 +0200

    The highscore now appears over time.

--- ADayAtStunlock/Assets/Scripts/Score/DisplayScoreList.cs
+++ ADayAtStunlock/Assets/Scripts/Score/DisplayScoreList.cs
@@ -28,13 +28,14 @@ public class DisplayScoreList : MonoBehaviour {
         {
             item.text = "-";
         }
-        DisplayScores();
-	}
+        //DisplayScores();
+        AnimateHighscoreList();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        DisplayScores();
+        //DisplayScores();
     }
 
     public void DisplayScores()
@@ -44,6 +45,53 @@ public class DisplayScoreList : MonoBehaviour {
             names[i].text = Highscore.scores[i].Name;
             scores[i].text = Highscore.scores[i].Amount.ToString("n0") + " PTS";
             times[i].text = Highscore.scores[i].Time.ToString("n0") + " s";
+        }
+    }
+    public void AnimateHighscoreList()
+    {
+        ClearHighscoreList();
+        StartCoroutine(TypeText());
+    }
+
+    private IEnumerator TypeText()
+    {
+        float speed = 0.05f;
+        for (int i = 0; i < Highscore.scores.Count; i++)
+        {
+            foreach (char letter in Highscore.scores[i].Name.ToCharArray())
+            {
+                names[i].text += letter;
+                yield return new WaitForSeconds(speed);
+            }
+            string score = Highscore.scores[i].Amount.ToString("n0") + " PTS";
+            foreach (char letter in score.ToCharArray())
+            {
+                scores[i].text += letter;
+                yield return new WaitForSeconds(speed);
+            }
+            string time = Highscore.scores[i].Time.ToString("n0") + " s";
+            foreach (char letter in time.ToCharArray())
+            {
+                times[i].text += letter;
+                yield return new WaitForSeconds(speed);
+            }
+        }
+    }
+
+    private void ClearHighscoreList()
+    {
+
+        foreach (Text name in names)
+        {
+            name.text = " ";
+        }
+        foreach (Text score in scores)
+        {
+            score.text = " ";
+        }
+        foreach (Text time in times)
+        {
+            time.text = " ";
         }
     }
 }
