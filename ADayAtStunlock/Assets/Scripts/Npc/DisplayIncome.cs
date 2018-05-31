639445566d4139ea738cde2b4c629be1d78781aa
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-05-31 16:27:01 +0200

    Added display of potential income. coming soon to a game near you!

---
+++ ADayAtStunlock/Assets/Scripts/Npc/DisplayIncome.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayIncome : MonoBehaviour {
+    TextMesh incomeText;
+	// Use this for initialization
+	void Start () {
+        incomeText = GetComponent<TextMesh>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    private void  UpdateDisplayedIncome()
+    {
+        //There are 7 states, gaining big, medium and small, break even, lose big medium and small amount of money.
+        //Break even -5 <-> 5 
+        //
+        //
+
+        private int m_breakEven       = 5;
+        private int m_smallAmount     = 10;
+        private int m_mediumAmount    = 15;
+        private int m_bigAmount       = 20;
+
+        if(MoneyManager.MoneyDifferenceLastGenerate <= 5 && 
+           MoneyManager.MoneyDifferenceLastGenerate >= -5)
+        {
+
+        }
+    }
+    
+}
