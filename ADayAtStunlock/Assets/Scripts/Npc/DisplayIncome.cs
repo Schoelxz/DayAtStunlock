c2b0fe5ad520958945c3e95e2c356dec43cab16b
Thomas Richards <thomas.c.richards1@gmail.com>
2018-06-01 11:22:03 +0200

    Game now displays a $ on the workstation screen that indicates if the player earns or loses money by standing at the workstation. I also hid the $$$ text thing arrow because it was in the way.

--- ADayAtStunlock/Assets/Scripts/Npc/DisplayIncome.cs
+++ ADayAtStunlock/Assets/Scripts/Npc/DisplayIncome.cs
@@ -5,34 +5,82 @@ using UnityEngine.UI;
 
 public class DisplayIncome : MonoBehaviour {
     TextMesh incomeText;
-	// Use this for initialization
-	void Start () {
+
+    [SerializeField] Color positiveColor;
+    [SerializeField] Color negativeColor;
+    [SerializeField] Color neutralColor;
+    // Use this for initialization
+    void Start() {
         incomeText = GetComponent<TextMesh>();
 
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDisplayedIncome();
     }
-    private void  UpdateDisplayedIncome()
+    private void UpdateDisplayedIncome()
     {
         //There are 7 states, gaining big, medium and small, break even, lose big medium and small amount of money.
-        //Break even -5 <-> 5 
-        //
-        //
-
-        private int m_breakEven       = 5;
-        private int m_smallAmount     = 10;
-        private int m_mediumAmount    = 15;
-        private int m_bigAmount       = 20;
-
-        if(MoneyManager.MoneyDifferenceLastGenerate <= 5 && 
-           MoneyManager.MoneyDifferenceLastGenerate >= -5)
+        //max value is 36, min value is -18. break even is at 0
+        //const float positiveBigAmount     = 36f;
+        const float positiveMediumAmount  = 18f;
+        const float positiveSmallAmount   = 6f;
+        const float breakEven             = 2f;
+        const float negativeSmallAmount   = -6f;
+        const float negativeMediumAmount  = -12f;                                
+        //const float negativeBigAmount     = -18f;                               
+        print(MoneyManager.PotentialMoneyDifference);  
+        if(MoneyManager.PotentialMoneyDifference > positiveMediumAmount)
         {
-
+            //Display +$$$
+            ChangeTextToNew("+$$$");
+            ChangeTextColor(positiveColor);
+        }
+        else if(MoneyManager.PotentialMoneyDifference > positiveSmallAmount)
+        {
+            //Display +$$
+            ChangeTextToNew("+$$");
+            ChangeTextColor(positiveColor);
+        }
+        else if(MoneyManager.PotentialMoneyDifference > breakEven)
+        {
+            //Display +$
+            ChangeTextToNew("+$");
+            ChangeTextColor(positiveColor);
+        }
+        else if(MoneyManager.PotentialMoneyDifference < breakEven && MoneyManager.PotentialMoneyDifference > -breakEven)
+        {
+            //Display |
+            ChangeTextToNew("|");
+            ChangeTextColor(neutralColor);
+        }
+        else if(MoneyManager.PotentialMoneyDifference > negativeSmallAmount)
+        {
+            //Display -$
+            ChangeTextToNew("-$");
+            ChangeTextColor(negativeColor);
+        }
+        else if(MoneyManager.PotentialMoneyDifference > negativeMediumAmount)
+        {
+            //Display -$$
+            ChangeTextToNew("-$$");
+            ChangeTextColor(negativeColor);
+        }
+        else
+        {
+            //Display -$$$
+            ChangeTextToNew("-$$$");
+            ChangeTextColor(negativeColor);
         }
     }
-    
+    private void ChangeTextToNew(string newText)
+    {
+        incomeText.text = newText;
+    }
+    private void ChangeTextColor(Color newColor)
+    {
+        incomeText.color = newColor;
+    }
 }
